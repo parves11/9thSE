34fa2ae857d31e0a702a17752e66ee7c53682176
parves <paparves11@gmail.com>
2018-08-30 04:01:17 -0400

    Initial commit

---
+++ src/hello/singletion/example/House.java
@@ -0,0 +1,24 @@
+package hello.singletion.example;
+
+
+public class House {
+
+    private House() {
+    }
+  //  public static House house1 = new House();
+
+
+   // private static House house1 = new House();
+
+    public static  House getSingletion() {
+      return new House();
+    }
+
+    public void displayHousedim() {
+        System.out.println(" 100 by 100");
+    }
+
+
+}
+
+
---
+++ src/hello/singletion/example/TestHouse.java
@@ -0,0 +1,8 @@
+package hello.singletion.example;
+
+public class TestHouse {
+    public static void main(String[] args) {
+House house1 = House.getSingletion();
+house1.displayHousedim();
+    }
+}
